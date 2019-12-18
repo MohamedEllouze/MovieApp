15c0f2a543e9b688c24772387a25f95333d15b15
MohamedEllouze <mohamed.ellouze21@gmail.com>
2019-12-18 10:29:37 +0100

    feature1 commit

---
+++ src/feature1.js
@@ -0,0 +1,13 @@
+class feature1 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div>
+            
+        </div> );
+    }
+}
+ 
+export default feature1;
